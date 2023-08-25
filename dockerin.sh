586ccc38f8f3de16a7b6daacec6c57dc20b59915
Saurabh Dahibhate <saurabhdahibhate50@gmail.com>
2023-08-25 18:19:20 +0530

    Update dockerin.sh

--- dockerin.sh
+++ dockerin.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Update package list
+sudo apt update
+
+# Install OpenJDK 17
+sudo apt install -y openjdk-17-jre
+
+# Check Java version
+java -version
+
+# Add Jenkins repository key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repository
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package list with Jenkins repository
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install -y jenkins
+
 # Update the apt package index and install necessary packages
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
